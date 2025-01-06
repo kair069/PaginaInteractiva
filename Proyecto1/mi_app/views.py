e95c4be33c44723c736ab648ab6682ddcd13ad6a
alex <acayllahu@unsa.edu.pe>
2025-01-06 14:35:49 -0500

    ultimoooooo

--- Proyecto1/mi_app/views.py
+++ Proyecto1/mi_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
 def home(request):
     return render(request, 'mi_app/home.html')
+
+def about(request):
+    return render(request, 'mi_app/about.html')
+
+def contact(request):
+    return render(request, 'mi_app/contact.html')
